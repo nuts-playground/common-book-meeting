2ef8b229d29174fe0d4c6d8ed09ef3bf50745404
kiyoung92 <go3go1go2@gmail.com>
2024-01-26 03:58:39 +0900

    test: test chapter3

--- 러닝-타입스크립트/jose/test.ts
+++ 러닝-타입스크립트/jose/test.ts
@@ -1,1 +1,17 @@
-let foo = 'hello';
+let foo: 'hello';
+let baz: string;
+
+foo = 'hello';  // ok
+
+foo = 'world';  // Error: Type '"world"' is not assignable to type '"hello"'.
+
+let bar = '';   // string type
+
+foo = bar;  // Error: Type 'string' is not assignable to type '"hello"'.
+
+baz = bar;  // ok
+
+foo = baz;  // Error: Type 'string' is not assignable to type '"hello"'.
+
+baz = foo;  // ok
+
